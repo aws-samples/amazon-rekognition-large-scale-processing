e8fcd71ed190a7a76a4c4d119065b5d60af8bddc
Gabriel Paredes <paredgab@amazon.com>
2020-09-10 17:05:09 -0500

    Version 1

--- README.md
+++ README.md
@@ -1,65 +1,46 @@
 
 # Welcome to your CDK Python project!
 
-You should explore the contents of this project. It demonstrates a CDK app with an instance of a stack (`rekognition_processing_app_stack`)
-which contains an Amazon SQS queue that is subscribed to an Amazon SNS topic.
+This is a blank project for Python development with CDK.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
-This project is set up like a standard Python project.  The initialization process also creates
-a virtualenv within this project, stored under the .env directory.  To create the virtualenv
-it assumes that there is a `python3` executable in your path with access to the `venv` package.
-If for any reason the automatic creation of the virtualenv fails, you can create the virtualenv
-manually once the init process completes.
+### Architecture
 
-To manually create a virtualenv on MacOS and Linux:
+![Architecture Diagram](docs/diagram_arch.png)
 
+### Usage
+
+#### Prerequisites
+
+- A AWS Account 
+- AWS CDK for Python
+- Python 3.6 or latter
+- IAM Privileges to deploy the components of the architecture
+
+#### Deployment
 ```
-$ python3 -m venv .env
-```
-
-After the init process completes and the virtualenv is created, you can use the following
-step to activate your virtualenv.
-
-```
-$ source .env/bin/activate
-```
-
-If you are a Windows platform, you would activate the virtualenv like this:
-
-```
-% .env\Scripts\activate.bat
+$ npm install -g aws-cdk
+$ cd <Mod>
+$ pip install -r requirements.txt    # Best to do this in a virtualenv
+$ cdk deploy                         # Deploys the CloudFormation template
 ```
 
-Once the virtualenv is activated, you can install the required dependencies.
-
+#### Cleanup
 ```
-$ pip install -r requirements.txt
-```
-
-At this point you can now synthesize the CloudFormation template for this code.
-
-```
-$ cdk synth
+cd <Mod>
+$ cdk destroy                        # This comand will delete all the deployed resources
 ```
 
-You can now begin exploring the source code, contained in the hello directory.
-There is also a very trivial test included that can be run like this:
+### Making changes to the code and customization
 
-```
-$ pytest
-```
+The [contributing guidelines](CONTRIBUTING.md) contains some instructions about how to run the front-end locally and make changes to the back-end stack.
 
-To add additional dependencies, for example other CDK libraries, just add to
-your requirements.txt file and rerun the `pip install -r requirements.txt`
-command.
+## Contributing
 
-## Useful commands
+Contributions are more than welcome. Please read the [code of conduct](CODE_OF_CONDUCT.md) and the [contributing guidelines](CONTRIBUTING.md).
 
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
 
-Enjoy!
+## License Summary
+
+This library is licensed under the MIT-0 License. See the LICENSE file.
--- rekognition_processing_app/rekognition_processing_app_stack.py
+++ rekognition_processing_app/rekognition_processing_app_stack.py
@@ -1,8 +1,29 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from aws_cdk import (
-    aws_iam as iam,
     aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_lambda_event_sources as _lambda_events,
+    aws_s3_notifications as s3n,
+    aws_iam as iam,
+    aws_dynamodb as dynamodb,
     core
 )
 
@@ -12,13 +33,78 @@ class RekognitionProcessingAppStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "RekognitionProcessingAppQueue",
+        # Create image bucket
+        image_bucket = s3.Bucket(self, 'inbound_image_s3_bucket')
+
+        # Create the image processing queue
+        image_process_queue = sqs.Queue(
+            self, "image_process_queue",
             visibility_timeout=core.Duration.seconds(300),
+            retention_period=core.Duration.days(1)
         )
 
-        topic = sns.Topic(
-            self, "RekognitionProcessingAppTopic"
+        # Create the image response queue
+        response_queue = sqs.Queue(
+            self, "results_queue",
+            visibility_timeout=core.Duration.seconds(300),
+            retention_period=core.Duration.days(1)
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # Set the put object notification to the SQS Queue
+        image_bucket.add_event_notification(event=s3.EventType.OBJECT_CREATED_PUT,
+                                            dest=s3n.SqsDestination(image_process_queue))
+
+        # Define the AWS Lambda to call Amazon Rekognition DetectFaces
+        detect_faces_lambda = _lambda.Function(self, 'detect_faces',
+                                               runtime=_lambda.Runtime.PYTHON_3_7,
+                                               handler='detect_faces.lambda_handler',
+                                               code=_lambda.Code.asset('./lambda'),
+                                               timeout=core.Duration.seconds(30),
+                                               environment={'SQS_RESPONSE_QUEUE': response_queue.queue_name},
+                                               reserved_concurrent_executions=50
+                                               )
+
+        # Set SQS image_process_queue Queue as event source for detect_faces_lambda
+        detect_faces_lambda.add_event_source(_lambda_events.SqsEventSource(image_process_queue,
+                                                                           batch_size=1))
+
+        # Allow response queue messages from lambda
+        response_queue.grant_send_messages(detect_faces_lambda)
+
+        # Allow lambda to call Rekognition by adding a IAM Policy Statement
+        detect_faces_lambda.add_to_role_policy(iam.PolicyStatement(actions=['rekognition:*'],
+                                                                   resources=['*']))
+        # Allow lambda to read from S3
+        image_bucket.grant_read(detect_faces_lambda)
+
+        # Define the DynamoDB Table
+        results_table = dynamodb.Table(self, 'detect_faces_results',
+                                       table_name='detect_faces_results',
+                                       partition_key=dynamodb.Attribute(name='id', type=dynamodb.AttributeType.STRING),
+                                       read_capacity=200,
+                                       write_capacity=200
+                                       )
+
+        # Define the AWS Lambda to write results into DyanamoDB results_table
+        write_results_lambda = _lambda.Function(self, 'write_results',
+                                               runtime=_lambda.Runtime.PYTHON_3_7,
+                                               handler='write_results.lambda_handler',
+                                               code=_lambda.Code.asset('./lambda'),
+                                               timeout=core.Duration.seconds(30),
+                                               environment={'TABLE_NAME': results_table.table_name}
+                                               )
+
+        # Set SQS response_queue Queue as event source for write_results_lambda results_table
+        write_results_lambda.add_event_source(_lambda_events.SqsEventSource(response_queue,
+                                                                            batch_size=1))
+
+        # Allow AWS Lambda write_results_lambda to Write to Dynamodb
+        results_table.grant_write_data(write_results_lambda)
+
+        # Allow AWS Lambda write_results_lambda to read messages from the SQS response_queue Queue
+        response_queue.grant_consume_messages(write_results_lambda)
+
+        # Output to Amazon S3 Image Bucket
+        core.CfnOutput(self, 'cdk_output',
+                       value=image_bucket.bucket_name,
+                       description='Input Amazon S3 Image Bucket')
